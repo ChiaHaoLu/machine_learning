39bc59c6dda8720930645159441808032f10956f
chlu <chlu@esl.cse.nsysu.edu.tw>
2018-01-25 03:02:41 +0800

    rbfn with center normalization

--- test_model.py
+++ test_model.py
@@ -3,13 +3,17 @@ import numpy as np
 from tf_rbf import TFRBFNet as RBFNet
 import load_data as ld
 
-data_size = 710315
-x_dim = 3
+data_size = 354694
+#data_size = 10
+x_dim = 34
 y_dim = 1
 train_x,train_y = ld.load_data(data_size,x_dim,y_dim,"data/train.csv")
 
+#train_x = train_x[0:10]
+#train_y = train_y[0:10]
+#print train_x
 rbf = RBFNet(k=10)
 rbf.fit(train_x, train_y)
 
-prediction = rbf.predict(train_x)
-#print prediction
+prediction = rbf.predict(train_x[1:100])
+print prediction
--- tf_rbf.py
+++ tf_rbf.py
@@ -1,7 +1,8 @@
 # !/usr/bin/env python
 # encoding: utf-8
 
-from tf_kmeans import TFKMeans
+#from tf_kmeans import TFKMeans
+from tensorflow.contrib.learn import KMeansClustering as tf_kmeans
 import tensorflow as tf
 
 
@@ -23,29 +24,35 @@ class  TFRBFNet ( object ):
             _max_iter: 迭代次數
             trainWithBeta: 第二種訓練方式
         '''
-        self ._delta = delta
-        self ._beta =  None
-        self ._input_n =  0
-        self ._hidden_num = k
-        self ._output_n =  0
-        self .max_iter =  5000
-        self .trainWithBeta =  False
-        self .sess = tf.Session()
+        #self._delta = tf.Variable(delta)
+	self._delta = delta
+        self._beta =  None
+        self._input_n =  0
+        self._hidden_num = k
+        self._output_n =  0
+        self.max_iter =  10
+        self.trainWithBeta =  False
+        self.sess = tf.Session()
 
     def  setup ( self , ni , nh , no ):
         '''
         網絡建立
         '''
-        self ._input_n = ni
-        self ._hidden_num = nh
-        self ._output_n = no
+        self._input_n = ni
+        self._hidden_num = nh
+        self._output_n = no
 
-        self .input_layer = tf.placeholder(tf.float32, [ None , self ._input_n], name = 'inputs_layer' )
-        self .output_layer = tf.placeholder(tf.float32, [ None , self ._output_n], name = 'outputs_layer' )
+        self.input_layer = tf.placeholder(tf.float32, [ None , self ._input_n], name = 'inputs_layer' )
+        self.output_layer = tf.placeholder(tf.float32, [ None , self ._output_n], name = 'outputs_layer' )
+        self.getHiddenCenters( self._inputs)
+        self.hidden_centers = tf.constant( self.hidden_centers, name = "hidden" )
+	
+	self.max_for_norm = tf.reduce_max(self.input_layer,axis=0)
+	small_value = tf.fill(self.max_for_norm.shape,0.0000000001)
+        self.max_for_norm = tf.where(tf.cast(self.max_for_norm,dtype=tf.bool), self.max_for_norm, small_value);
 
-        self .getHiddenCenters( self ._inputs)
-        self .hidden_centers = tf.constant( self .hidden_centers, name = "hidden" )
-        self .hidden_layer =  self .rbfunction( self .input_layer, self .hidden_centers)
+	self.hidden_layer =  self.rbfunction( self.input_layer, self.hidden_centers)
+
 
     def  fit ( self , inputs , outputs ):
         '''
@@ -58,6 +65,7 @@ class  TFRBFNet ( object ):
         self.setup(inputs.shape[ 1 ], self ._hidden_num, outputs.shape[ 1 ])
 
         self.sess.run(tf.global_variables_initializer())
+	print 'fit'
         if  self .trainWithBeta:
             self .LinearTrain()
         else :
@@ -81,19 +89,19 @@ class  TFRBFNet ( object ):
         '''
         梯度下降法訓練RBF隱層->輸出層的參數
         '''
-        self .trainWithBeta =  False
-
+        self.trainWithBeta =  False
+	print 'start_train'
         #最後預測的輸出
-        self .predictionWithGD =  self .addLayer( self .hidden_layer, self ._hidden_num, self ._output_n)
+        self.predictionWithGD =  self.addLayer( self.hidden_layer, self._hidden_num, self ._output_n)
         #平方損失誤差
-        self .loss = tf.reduce_mean(tf.square( self .predictionWithGD -  self .output_layer))
+        self.loss = tf.reduce_mean(tf.square( self.predictionWithGD -  self.output_layer))
         #梯度下降優化
-        self .optimizer = tf.train.GradientDescentOptimizer( 0.1 ).minimize( self .loss)
-
+        self.optimizer = tf.train.GradientDescentOptimizer( 0.1 ).minimize( self .loss)
+	print 'start_loop'
         self .sess.run(tf.global_variables_initializer())
         for i in  range ( self .max_iter):
             self .sess.run( self .optimizer, feed_dict = { self .input_layer: self ._inputs, self .output_layer: self ._outputs})
-            if i % 1000  ==  0 :
+            if i % 1  ==  0 :
                 print  ' iter: ' ,i, ' loss: ' , self .sess.run( self .loss, feed_dict = { self .input_layer: self ._inputs, self .output_layer: self ._outputs})
 
 
@@ -127,11 +135,20 @@ class  TFRBFNet ( object ):
         使用TF版的Kmeans基於歐式距離進行無監督聚類
         獲得中心
         '''
-        kms = TFKMeans( self ._hidden_num, session = self .sess)
-        kms.train(tf.constant(inputs))
-        self .hidden_centers = kms.centers
+        kms = tf_kmeans(self._hidden_num,initial_clusters='kmeans_plus_plus')
+	def input_for_kmeans():
+	        data = tf.constant(inputs,tf.float32)
+        	return (data,None)
+	kms.fit(input_fn=input_for_kmeans,max_steps=100)
+	self.hidden_centers = kms.clusters()
+	print 'kmeans finished'
+	#kms = TFKMeans( self._hidden_num, session = self.sess)
+        #kms.train(tf.constant(inputs))
+        #self.hidden_centers = kms.centers
 
     def  rbfunction ( self , x , c ):
+	c = tf.divide(c, self.max_for_norm)
+	x = tf.divide(x, self.max_for_norm )
         e_c = tf.expand_dims(c, 0 )
         e_x = tf.expand_dims(x, 1 )
-        return tf.exp( - self ._delta * tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(e_c, e_x)), 2 )))
+        return tf.exp( - self._delta * tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(e_c, e_x)), 2 )))
