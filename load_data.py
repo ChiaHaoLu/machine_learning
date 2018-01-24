adc5d65368822528a33a77d39ee41cfcefca7acb
chlu <chlu@esl.cse.nsysu.edu.tw>
2018-01-25 03:05:20 +0800

    small modify

--- load_data.py
+++ load_data.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import tensorflow as tf
 import sys
 
 #data_size = int(sys.argv[1])
@@ -19,21 +20,19 @@ def load_data(data_size, x_dim, y_dim, text_path):
 	row = csv.reader(text , delimiter=",")
 
 	for r_data in row:
-	    if n_row != 0:
-        	for i in range(x_dim+y_dim):
-			if i < y_dim:
-                		data_y[n_row-1].append(float(r_data[i]))
-			else:
-				data_x[n_row-1].append(float(r_data[i]))
-#	data_x[n_row-1].append(float(1))			
-	    n_row = n_row+1
-
+		if n_row != 0:
+        		for i in range(x_dim+y_dim):
+				if i < y_dim:
+                			data_y[n_row-1].append(float(r_data[i]))
+				else:
+					data_x[n_row-1].append(float(r_data[i]))
+		n_row = n_row+1
+		if n_row > data_size :
+			break
 	text.close()
-	data_y = np.array(data_y)
-	data_x = np.array(data_x)
 
-	data_x = data_x.astype('float32')
-	data_y = data_y.astype('float32')
+	data_x = np.array(data_x,dtype='float32')
+	data_y = np.array(data_y,dtype='float32')
 	print 'data x shape: ',data_x.shape
 	print 'data y shape: ',data_y.shape
 	return data_x, data_y
